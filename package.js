fee476469fdcdaa68c7dfc528bd2c11ee5a5c5c0
Kristoffer Klintberg <krstffr@gmail.com>
2015-08-07 15:28:43 +0200

    Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: "krstffr:reactive-constructor-cms",
 	summary: "CMS for krstffr:reactive-constructor instances.",
-	version: "0.3.3"
+	version: "0.3.4"
 });
 
 Package.onUse(function (api) {
