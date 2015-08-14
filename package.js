cdc5d89eff2e34f2861b3674d07f89bdb5ee14d2
Kristoffer Klintberg <krstffr@gmail.com>
2015-08-14 14:36:59 +0200

    Added published "flag" in list of docs

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: "krstffr:reactive-constructor-cms",
 	summary: "CMS for krstffr:reactive-constructor instances.",
-	version: "0.4.0"
+	version: "0.4.1"
 });
 
 Package.onUse(function (api) {
